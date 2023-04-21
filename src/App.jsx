a813e2753a835b068fdbc5c5e88f09ddf92766c4
Brandon <brandonyuh@gmail.com>
2023-04-20 20:04:50 -0700

    added encoded contact info

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import "./App.scss";
 
 function App() {
-  const skills = ["ReactJS", "NodeJS", "ExpressJS", "MySQL", "HTML", "CSS", "JavaScript", "Bootstrap", "Git", "GitHub", "Heroku", "Netlify", "VS Code", "Postman", "C#", "Java", "Python", "Unity", "C++", "C", "Linux", "Windows", "MacOS", ];
+  const encEmail = "YnJhbmRvbnl1aEBnbWFpbC5jb20=";
+  const encPhone = "MTc3ODcwOTUyMzk=";
+
+  const skills = ["ReactJS", "NodeJS", "ExpressJS", "MySQL", "HTML", "CSS", "JavaScript", "SASS", "Bootstrap", "Git", "GitHub", "Heroku", "Netlify", "VS Code", "Postman", "C#", "Java", "Python", "Unity", "C++", "C", "PHP", "JQuery", "Netbeans", "AdoDB", "Android Studio", "Vector Graphics", "JSP", "Axure", "Eclipse", "Subversion", "Mercurial", "Perforce", "BlackBerry API", "Google Maps API"];
+
   return (
     <>
       <h1>Brandon Yuh</h1>
+      <div>
+        <a href={"mailto:".concat(atob(encEmail))}>Email: {atob(encEmail)}</a>
+      </div>
+      <div>
+        <a href={"tel:".concat(atob(encPhone))}>Phone: {atob(encPhone)}</a>
+      </div>
       <h2>
         TechStack = <span className="bracket">[</span>
       </h2>
